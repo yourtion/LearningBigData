252abf7c2f2780464fac9c91cd6d805cefdb340d
YourtionGuo <yourtion@gmail.com>
2019-04-02 12:41:53 +0800

    add file delete and Combiner

--- Hadoop101/src/main/java/com/yourtion/hadoop/WordCountApp.java
+++ Hadoop101/src/main/java/com/yourtion/hadoop/WordCountApp.java
@@ -1,4 +1,81 @@
 package com.yourtion.hadoop;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * @author Yourtion
+ */
 public class WordCountApp {
+
+    public final static LongWritable one = new LongWritable(1);
+
+    public static class MyMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value
+                    .toString();
+
+            String[] words = line.split(" ");
+
+            for (String word : words) {
+                context.write(new Text(word), one);
+            }
+        }
+    }
+
+    public static class MyReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
+        @Override
+        protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+            long sum = 0;
+
+            for (LongWritable v : values) {
+                sum += v.get();
+            }
+
+            context.write(key, new LongWritable((sum)));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration configuration = new Configuration();
+
+        Path outputPath = new Path(args[1]);
+
+        FileSystem fs = FileSystem.get(configuration);
+        if (fs.exists(outputPath)) {
+            fs.delete(outputPath, true);
+        }
+
+        Job job = new Job(configuration, "WordCount");
+
+        job.setJarByClass(WordCountApp.class);
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+
+        job.setMapperClass(MyMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LongWritable.class);
+
+        job.setReducerClass(MyReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        job.setCombinerClass(MyReducer.class);
+
+        FileOutputFormat.setOutputPath(job, outputPath);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+
+    }
 }
